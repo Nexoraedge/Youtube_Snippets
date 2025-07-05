dbca8652726ff016c3190e3dc5065c79aba9b8ac
Nexoraedge <onlyego1043@gmail.com>
2025-07-05 10:10:29 +0530

    img section

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'platform-lookaside.fbsbx.com', 
       },
+      {
+        protocol: 'https',
+        hostname: 'xafyqdmsvmpkdzjfnmzl.supabase.co',
+      },
       { protocol: 'https', hostname: 'pbs.twimg.com' },
       
     ],
